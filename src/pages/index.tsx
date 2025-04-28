3dd836ce1a86a152ebd8ed3144116650110ca929
dicerollcodes <justinbustamantemacias@gmail.com>
2025-04-28 09:06:37 -0400

    changed around some bio stuff :p

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,9 +29,9 @@ export default function Home() {
               text-base text-gray-400 transform transition-all duration-700
               ${showContent ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}
             `} style={{ transitionDelay: '200ms' }}>
-              I'm Justin Bustamante, an aspiring software engineer planning to major in computer science and math. 
-              I love building computers, playing video games (currently obsessed with Marvel Rivals), 
-              and blasting music whenever I can. My favorite artists are Travis Scott, beabadoobee, and Daniel Caesar.
+              I'm Justin Bustamante, an aspiring software engineer major in computer science and math at Rutgers University-New Brunswick. 
+              I love building computers, playing video games (currently addicted to Overwatch 2 & Valorant but learning how to play Tekken 8), 
+              and listening to music. My current favorite artists are The Marias, beabadoobee, and .
             </p>
           </div>
           
